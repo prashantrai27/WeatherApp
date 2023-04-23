acd725daa687c27cac352d7f903ab37d6ef540cb
prashantrai27 <prashantrai317@gmail.com>
2023-04-23 17:27:33 +0530

    City selection option added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,6 +13,7 @@ import { WeatherMainComponentComponent } from './components/weather-main-compone
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     FontAwesomeModule
   ],
--- src/app/components/weather-main-component/weather-main-component.component.ts
+++ src/app/components/weather-main-component/weather-main-component.component.ts
@@ -1,31 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {faSun, faMoon} from '@fortawesome/free-regular-svg-icons'
+import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
 @Component({
   selector: 'app-weather-main-component',
   templateUrl: './weather-main-component.component.html',
   styleUrls: ['./weather-main-component.component.scss']
 })
-export class WeatherMainComponentComponent implements OnInit{
-  public weatherData:any;
+export class WeatherMainComponentComponent implements OnInit {
+  public weatherData: any;
   constructor(private http: HttpClient) { }
   sunIcon = faSun;
   moonIcon = faMoon;
+  city = "seoni"
   ngOnInit(): void {
     this.weatherData = {
       main: {},
-      isDay:true
+      isDay: true
     }
-    this.getWeatherData();
+    this.getWeatherData(this.city);
   }
 
-  getWeatherData() {
-        this.http.get('https://api.openweathermap.org/data/2.5/weather?q=seoni&appid=ff1bc4683fc7325e9c57e586c20cc03e').subscribe((data)=>{
-          this.setWeatherData(data);
-        })
+  getWeatherData(city: string) {
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ff1bc4683fc7325e9c57e586c20cc03e').subscribe((data) => {
+      this.setWeatherData(data);
+    })
   }
 
-  setWeatherData(data:any){
+  setWeatherData(data: any) {
     this.weatherData = data;
     let sunsetTime = new Date(this.weatherData.sys.sunset * 1000);
     this.weatherData.sunset_time = sunsetTime.toLocaleTimeString();
@@ -39,6 +40,11 @@ export class WeatherMainComponentComponent implements OnInit{
     this.weatherData.city = this.weatherData.name;
 
 
+  }
+
+  citySubmit(item: any) {
+    this.city = item.citySelected;
+    this.getWeatherData(this.city);
   }
 
 }
